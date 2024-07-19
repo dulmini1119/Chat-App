f993053e43abeb364cd98172b683756dc1d8e8c9
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-19 23:57:50 +0530

    Initial commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,45 @@
-export const signup = (req,res)=>{
+import User from "../models/user.model.js";
+
+export const signup = async (req,res) =>{
     try {
         const {fullName,username,password,confirmPassword,gender} = req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({message:"Password and Confirm Password does not match."});
+        }
+
+        const user = await User.findOne({username});
+
+        if(user){
+            return res.status(400).json({message:"Username already exists."});
+        }
+
+        //hash password
+
+        //profilepicture
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic
+        });
+
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
     }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,17 @@ import authRoutes from "./routes/auth.routes.js";
 import connecttoMongoDB from './db/connecttoMongoDB.js';
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5001;
 
+dotenv.config();
 
-app.get("/",(req,res)=> {
-    res.send("Hello World!");
-});
-
+app.use(express.json());
 app.use("/api/auth", authRoutes)
+
+// app.get("/",(req,res)=> {
+//     res.send("Hello World!");
+// });
+
 
 app.listen(PORT, ()=> {
     connecttoMongoDB();
