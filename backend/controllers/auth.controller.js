e6220d91cd6b63053af18a78ff9baa46cd446f01
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-21 09:11:26 +0530

    commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,7 @@
+import bcrypt from 'bcryptjs';
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from '../utils/generateToken.js';
+
 
 export const signup = async (req,res) =>{
     try {
@@ -15,6 +18,9 @@ export const signup = async (req,res) =>{
         }
 
         //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
 
         //profilepicture
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -23,19 +29,25 @@ export const signup = async (req,res) =>{
         const newUser = new User({
             fullName,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
 
-        await newUser.save();
-
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        });
+        
+        if(newUser){
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            });
+        }else{
+            res.status(400).json({message:"Something went wrong."});
+        }
+        
 
     } catch (error) {
         console.log("Error in signup controller", error.message);
@@ -43,10 +55,36 @@ export const signup = async (req,res) =>{
     }
 };
 
-export const login = (req,res)=>{
-    console.log('login');
+export const login = async (req,res)=>{
+    try {
+        const {username,password} = req.body;
+        const user = await User.findOne({username});
+        const ifPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !ifPasswordCorrect){
+            return res.status(400).json({message:"Invalid username or password."});
+        }
+        generateTokenAndSetCookie(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic
+            });
+
+    } catch (error) {
+      console.log("Error in login controller", error.message);
+      res.status(500).json({error: "Internal Server Error"});
+    }
 };
 
 export const logout = (req,res)=>{
-    console.log('logout');
+   try {
+    res.status("jwt","",{maxAge:0});
+    res.status(200).json({message:"Logged out successfully."});
+    
+   } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({error: "Internal Server Error"});
+   }
 };
---
+++ backend/models/message.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId:{
+        type: String,
+        ref:"User",
+        required: true
+    },
+    receiverId:{
+        type: String,
+        ref:"User",
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+},{timestamps:true});
+
+const message = mongoose.model("Message", messageSchema);
+export default message;
