689e8c2d577b2cfd3347343e1c8e5bf7e1f65baa
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-22 21:59:10 +0530

    Initial commit

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,8 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 15* 24* 60* 60* 1000,
         httpOnly: true, //prevent cross-site scripting attacks
-        sameSite:"strict" //CSRF attacks cross-site request forgery attacks
+        sameSite:"strict", //CSRF attacks cross-site request forgery attacks
+        secure:process.env.NODE_ENV !== "development"
     })
 }
 export default generateTokenAndSetCookie;
