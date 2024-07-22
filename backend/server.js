b177d42a22423133dfae84c65faabec5ac946285
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-22 21:31:21 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connecttoMongoDB from './db/connecttoMongoDB.js';
 
 const app = express();
@@ -11,6 +12,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 // app.get("/",(req,res)=> {
 //     res.send("Hello World!");
