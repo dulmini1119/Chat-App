7271cfd639b859060d05f58ce07ef6838694859e
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-24 23:34:45 +0530

    Commit

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -27,13 +27,31 @@ export const sendMessage = async (req,res) => {
             conversation.messages.push(newMessage._id);
         }
 
-        await conversation.save();
-        await newMessage.save();
+        // await conversation.save();
+        // await newMessage.save();
 
+        await Promise.all([conversation.save(), newMessage.save()]);
         res.status(201).json(newMessage);
 
     } catch (error) {
         console.log("Error in sendMessage controller" , error.message);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+
+
+export const getMessage = async (req,res) => {
+    try {
+        const {id:userToChatId} = req.params;
+        const senderId = req.user._id;
+        const conversation = await Conversation.findOne({
+            participants:{ $all: [senderId, userToChatId]},
+            }).populate("messages");
+
+            res.status(200).json(conversation.messages);
+
+    } catch (error) {
+        console.log("Error in getMessage controller", error.message);
         res.status(500).json({error:"Internal server error"});
     }
 }
