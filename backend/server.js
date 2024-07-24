d18f6ea875c388e3fc9609a085efbedb9fd2b3d1
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-07-25 00:13:51 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
-app.use("api/users",userRoutes)
+app.use("/api/users",userRoutes)
 
 // app.get("/",(req,res)=> {
 //     res.send("Hello World!");
