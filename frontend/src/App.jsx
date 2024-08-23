0dfa2c63d4f92aacbd6c61beeb12c669185f0698
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-08-23 23:24:23 +0530

    Initial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./pages/home/Home.jsx"
 import Login from "./pages/login/Login.jsx"
 import SignUp from "./pages/signup/SignUp.jsx"
 
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <SignUp/>
+      <Home/>
     </div>
   
   
