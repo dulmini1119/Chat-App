7dc01fd28e20b00f25874a3fe1636c18834df5c0
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-08-31 23:17:35 +0530

    Initial Commit

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import useConversation from "../../zustand/useConversation"
 
-const Conversation = () => {
+
+const Conversation = ({conversation,lastIdx,emoji}) => {
+
+  const {selectedConversation, setSelectedConversation} = useConversation();
+  const isSelected = selectedConversation?._id === conversation._id;
+
   return (
     <>
-    <div className='flex gap-2 items-center hover:bg-sky-500 roundeed p-2 py-1 cursor-pointer'>
+    <div className={`flex gap-2 items-center hover:bg-sky-500 roundeed p-2 py-1 cursor-pointer
+      ${isSelected ? "bg-sky-500" : ""}
+      `}
+      onClick={() => setSelectedConversation(conversation)}
+      
+      >
 
     <div className='avatar online'>
         <div className='w-12 rounded-full'>
             <img
-            src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+            src={conversation.profilePic}
             alt='user avatar'
             />
         </div>
@@ -16,15 +26,15 @@ const Conversation = () => {
 
     <div className='flex flex-col flex-1'>
         <div className='flex gap-3 justify-between'>
-            <p className='font-bold text-gray-200'>John Doe</p>
-            <span className='text-xl'>👽</span>
+            <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+            <span className='text-xl'>{emoji}</span>
         </div>
     </div>
 
     </div>
 
 
-    <div className='divider my-0 py-0 h-1'/>
+    {!lastIdx && <div className='divider my-0 py-0 h-1'/>}
     </>
   )
 }
