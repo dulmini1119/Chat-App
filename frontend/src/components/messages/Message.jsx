0b78fd65ab7aec82859bdbdfbe16e6bf0c0fd629
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-09-02 23:50:06 +0530

    Initial Commit

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
 
-const Message = () => {
+import { useAuthContext } from '../../context/AuthContext';
+import { extractTime } from '../../utils/extractTime';
+import useConversation from '../../zustand/useConversation';
+
+const Message = ({message}) => {
+
+  const {authUser} = useAuthContext();
+  const {selectedConversation} = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const formatedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-blue-500': "";
+
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
                 <img
-                src={'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'}/>
+                src={profilePic}/>
             </div>
         </div>
 
-    <div className={`chat-bubble text-white bg-blue-500`}>Hi! what is up?</div>
-    <div className='chat-footer opacity-50 text-xs flex gap-1 items-center '>12.44</div>
+    <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+    <div className='chat-footer opacity-50 text-xs flex gap-1 items-center '>{formatedTime}</div>
 
     </div>
   );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5001",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:5001",
+			},
+		},
+	},
 });
