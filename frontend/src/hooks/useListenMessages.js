49846db0e95fe22519d8d888cd5ff637255e8748
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-09-03 02:33:51 +0530

    Initial Commit

---
+++ frontend/src/hooks/useListenMessages.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useSocketContext } from "../context/SocketContext";
+import useConversation from "../zustand/useConversation";
+
+import notificationSound from '../assets/sounds/notification.mp3'
+
+const useListenMessage = () => {
+    const { socket } = useSocketContext();
+    const { messages, setMessages } = useConversation();
+
+    useEffect(() => {
+        const handleNewMessage = (newMessage) => {
+            newMessage.shouldShake = true;
+            const sound = new Audio(notificationSound);
+            sound.play();
+            setMessages((prevMessages) => [...prevMessages, newMessage]);
+        };
+
+        socket?.on("newMessage", handleNewMessage);
+
+        return () => {
+            socket?.off("newMessage", handleNewMessage);
+        };
+    }, [socket, setMessages]);
+};
+
+export default useListenMessage;
